7beeb742264909114cfd0487e8ec5658bb66d285
Jonathan Ruiz <kioz.developer@gmail.com>
2021-05-26 20:48:37 -0500

    Create a map for earthquake data

---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,17 @@
+
+// Making a map
+var usaMap = L.map("map", {
+    center: [40.81, -97.76],
+    zoom: 5
+  });
+  
+  // Adding a tile layer
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(usaMap);
+  
