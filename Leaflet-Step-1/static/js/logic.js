8247faa93df9aa468c26268e686c81b0a5017296
Jonathan Ruiz <kioz.developer@gmail.com>
2021-05-27 08:15:34 -0500

    Fetching last 7 days data

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,3 +1,3 @@
-d3.json("https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2021-01-01&endtime=2021-01-02").then(function (earthquakeData) {
+d3.json("https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2021-05-20&endtime=2021-05-27").then(function (earthquakeData) {
     console.log(earthquakeData);
 });
