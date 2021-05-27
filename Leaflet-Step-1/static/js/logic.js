e85ea0b2898bad890e8f22e14408103c7c4991cf
Jonathan Ruiz <kioz.developer@gmail.com>
2021-05-27 08:11:29 -0500

    Adding d3 to fetch data

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,17 +1,3 @@
-
-// Making a map
-var usaMap = L.map("map", {
-    center: [40.81, -97.76],
-    zoom: 5
-  });
-  
-  // Adding a tile layer
-  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
-    accessToken: API_KEY
-  }).addTo(usaMap);
-  
+d3.json("https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2021-01-01&endtime=2021-01-02").then(function (earthquakeData) {
+    console.log(earthquakeData);
+});
